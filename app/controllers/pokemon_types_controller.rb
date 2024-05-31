688c1ae8d8cce3b7a3506957f82e5b73930b2b82
Andre Menezes <djdedemenezes@gmail.com>
2024-05-30 21:10:49 -0300

    reorder

--- app/controllers/pokemon_types_controller.rb
+++ app/controllers/pokemon_types_controller.rb
@@ -7,23 +7,24 @@ class PokemonTypesController < ApplicationController
   def create
     @pokemon = Pokemon.find(params[:pokemon_id])
 
-    # @pokemon_type = PokemonType.new()
+    # @pokemon_type = PokemonType.new(pokemon_type_params)
     # @pokemon_type.pokemon = @pokemon
     # raise
+    # if @pokemon_type.save
+    # else
+    #   render 'new', status: :unprocessable_entity
+    # end
+
     types = Type.where(id: params[:pokemon_type][:type])
     types.each do |type|
       PokemonType.create(pokemon: @pokemon, type: type)
     end
     redirect_to trainer_path(@pokemon.trainer)
-    # if @pokemon_type.save
-    # else
-    #   render 'new', status: :unprocessable_entity
-    # end
   end
 
   private
 
   def pokemon_type_params
-    params.require(:pokemon_type).permit()
+    params.require(:pokemon_type).permit(:type_id)
   end
 end
